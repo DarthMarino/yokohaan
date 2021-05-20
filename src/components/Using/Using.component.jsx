7e60bb56703a4f9bf060fa199a62e69723a618d7
Marino <marinogomez24@gmail.com>
2021-05-20 14:05:04 -0400

    Adding animations

--- src/components/Using/Using.component.jsx
+++ src/components/Using/Using.component.jsx
@@ -1,21 +1,36 @@
-import React, { useRef, Suspense } from "react";
-
+import React, { useRef, useState, Suspense } from "react";
+import { TweenMax } from "gsap";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "./Using.css";
 import { Euler } from "three";
 
 const Tablet = () => {
+  const [, setHover] = useState(null);
+  const [, setTilt] = useState(null);
   const gltf = useLoader(GLTFLoader, "models/tablet.glb");
   const nodes = gltf.nodes;
   const globalPosition = [-15, -12, -5];
   const globalAxis = new Euler(0, 600, 0, "XYZ");
   const group = useRef();
 
-  useFrame((state, delta) => {
-    group.current.position.y = Math.sin(state.clock.elapsedTime) / 2;
-    group.current.rotation.y = Math.sin(state.clock.elapsedTime) / 8;
+  useFrame((state) => {
+    setHover(
+      TweenMax.to(group.current.position, {
+        y: Math.sin(2 * state.clock.elapsedTime) / 2,
+        repeat: -1,
+        ease: "circ",
+      })
+    );
+    setTilt(
+      TweenMax.to(group.current.rotation, {
+        y: Math.sin(1.5 * state.clock.elapsedTime) / 8,
+        repeat: -1,
+        ease: "stepped",
+      })
+    );
   });
+
   return (
     <>
       <group ref={group} dispose={null}>
@@ -71,6 +86,7 @@ const Using = () => {
         <div className="tablet">
           <Canvas
             camera={{
+              aspect: window.innerWidth / window.innerHeight,
               position: [0, 0, 10],
             }}
           >
